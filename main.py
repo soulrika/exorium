7cb87d5c1cc3b8baf7f122d7801b41e65a4756ac
Bluewy <bluewyechache@gmail.com>
2020-10-17 08:52:50 +0200

    Removed bonk command
    On phone, coding is not efficient at all.

--- main.py
+++ main.py
@@ -278,11 +278,6 @@ async def boop(ctx, members: commands.Greedy[discord.Member], *, reason="being a
     await functions.interactions(ctx, members, reason, "boop", "so soft", "booped")
     await functions.logging(ctx, "boop", bot)
 
-@bot.command(name='bonk', brief='bonking, that's mean!')  # interaction command - snuggle someone. gifs are random!
-async def bonk(ctx, members: commands.Greedy[discord.Member], *, reason="being bad"):
-    await functions.interactions(ctx, members, reason, "bonk", "how rude", "bonked")
-    await functions.logging(ctx, "snuggle", bot)
-
 
 @bot.command(name='kiss', aliases=['smooch'], brief='Smooch!')  # interaction command - kiss someone. gifs are random!
 async def kiss(ctx, members: commands.Greedy[discord.Member], *, reason="being lovely"):
