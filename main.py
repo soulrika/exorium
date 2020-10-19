b28b43a2d1cd0ef1b74208fdbe3b5fceb586de3d
Bluewy <bluewyechache@gmail.com>
2020-10-19 14:33:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -229,8 +229,8 @@ async def serverinfo(ctx):
         "WELCOME_SCREEN_ENABLED": "Welcome Screen"
     }
 
+    print('successfully made it to the loop, i will now stop working.')
     for feature in gu.features:
-        print(feature)
         featuresText += f"\n{features[feature]}"
 
     if ctx.guild.features:
