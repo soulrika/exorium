580dbccc0fff60b5d4e4cdcde91537672bd9fbee
Bluewy <bluewyechache@gmail.com>
2020-11-27 22:54:31 +0100

    Hyperlinked whole news value

--- main.py
+++ main.py
@@ -87,12 +87,12 @@ botdesc = f'{support} | {invite} | {review} | {policy}'
 async def help(ctx):
     stats = f"**statistics:** {str(len(bot.guilds))} guilds | {str(len(bot.users))} users"
     discordpy = f"**discord.py version:** {discord.__version__}"
-    news = "Changed help command completely. Bot was transferred to a new VPS, for issues please join the support server."
+    news = "[Changed help command completely. Bot was transferred to a new VPS, for issues please join the support server.](https://twitter.com/exoriumbot)"
     if ctx.invoked_subcommand is None:
         e = discord.Embed(title=f'help cmd | prefix: `{ctx.prefix}`', description=botdesc, color=config.color)
         e.add_field(name='info', value=f'**Developers:** [Bluewy](https://discord.com/users/698080201158033409) | [Toothless](https://discord.com/users/341988909363757057)\n{stats}\n{discordpy}')
         e.add_field(name='categories', value=f'`{ctx.prefix}help social`\n**{ctx.prefix}help mod**\n`{ctx.prefix}help utils`\n**{ctx.prefix}help bot**\n`{ctx.prefix}help jsk (Dev+)`\n**These do not work yet.\nlook on the [GitHub repo](https://github.com/ThePawKingdom/exorium/blob/master/main.py) for the commands.**')
-        e.add_field(name='[Latest news](https://twitter.com/exoriumbot)', value=news)
+        e.add_field(name='Latest news', value=news)
         await ctx.send(embed=e)
 
 @help.command()
