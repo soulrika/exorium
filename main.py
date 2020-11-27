3554cc70ba6a436e75fa7f484f44257769d0e5d8
Chosen-Fate <55001472+Chosen-Fate@users.noreply.github.com>
2020-11-27 22:14:19 +0100

    fix help

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ async def help(ctx):
     stats = f"**statistics:** {str(len(bot.guilds))} guilds | {str(len(bot.users))} users"
     if ctx.invoked_subcommand is None:
         e = discord.Embed(title=f'help cmd | prefix: `{ctx.prefix}`', description=botdesc, color=config.color)
-        e.add_field(name=' ', value=f'**Developers:** [Bluewy](https://discord.com/users/698080201158033409) | [Toothless](https://discord.com/users/341988909363757057)\n{stats}')
+        e.add_field(name='test', value=f'**Developers:** [Bluewy](https://discord.com/users/698080201158033409) | [Toothless](https://discord.com/users/341988909363757057)\n{stats}')
         await ctx.send(embed=e)
 
 @help.command()
