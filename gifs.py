ed22f7126eb9222d5db65dcb5c548d93fca917c8
Bluewy <bluewyechache@gmail.com>
2020-12-06 12:15:41 +0100

    Added more slap gifs

--- gifs.py
+++ gifs.py
@@ -127,7 +127,10 @@ bonk = ["https://media1.tenor.com/images/c7db074a1f169d09f037e4a1695ce8ab/tenor.
         "https://media1.tenor.com/images/c70e5c67f32b126e8d62bb1ee099aec4/tenor.gif?itemid=17872492",
         "https://media1.tenor.com/images/5be9d9780f7f36146eef8ec6716493a5/tenor.gif?itemid=17119858"]
 
-slap = ["https://media1.tenor.com/images/1ed93f40b89cab10e8f5328f180f1241/tenor.gif?itemid=16415016"]
+slap = ["https://media1.tenor.com/images/1ed93f40b89cab10e8f5328f180f1241/tenor.gif?itemid=16415016",
+        "https://media1.tenor.com/images/37bc5bf5e95aa6afcd66fd779b923af5/tenor.gif?itemid=9629173",
+        "https://media1.tenor.com/images/13826f5f3060ce41d8fbe4d16a7acb63/tenor.gif?itemid=5428402",
+        "https://media1.tenor.com/images/415b6bd8c43eb6e8bf660f86b7b98581/tenor.gif?itemid=5730795"]
 
 votechannels = [731848766327619586, 731829759146721320]
 
