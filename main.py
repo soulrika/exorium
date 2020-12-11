9a761d425eca5deb298468d36978701e8ae82a61
Bluewy <bluewyechache@gmail.com>
2020-12-11 13:00:42 +0100

    remove useless imports

--- main.py
+++ main.py
@@ -6,8 +6,6 @@ import requests
 import logging
 import aiohttp
 import json
-import discord.opus
-import discord.voice_client
 import discord.ext
 from discord.ext import commands
 from collections import Counter
