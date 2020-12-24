f0a6caa65325856181b93e2ec07e0c0eea9d14d1
Bluewy <bluewyechache@gmail.com>
2020-12-24 15:38:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -179,8 +179,8 @@ async def nsfw(ctx):
 async def emote(ctx):
     if ctx.invoked_subcommand is None:
         e = discord.Embed(color=config.color)
-        e.description = f"**Info:**\nUse these handy commands to manage- and improve your use of emotes.\n\n**Commands:**\n- `convert` | Convert emote to URL\n`{ctx.prefix}emote convert <emote>`"
-        e.set_footer(text=ctx.message.author)
+        e.description = f"**Info:**\nUse these handy commands to manage- and improve your use of emotes.\n\n**Commands:**\n- `convert` | Convert emote to URL | `{ctx.prefix}emote convert <emote>`"
+        e.set_footer(text=f"Command executed by {ctx.message.author}")
         await ctx.send(embed=e)
         await functions.logging(ctx, "emote", bot)
         
