9258ba4bf2d588df04a6b1abe8d79c3179cf7f81
Bluewy <bluewyechache@gmail.com>
2020-12-27 16:50:22 +0100

    Updated news tab in help

--- main.py
+++ main.py
@@ -104,7 +104,7 @@ botdesc = f'{support} | {invite} | {review} | {policy}'
 async def help(ctx):
     stats = f"**statistics:** {str(len(bot.guilds))} guilds | {str(len(bot.users))} users"
     discordpy = f"**discord.py version:** {discord.__version__}"
-    news = f"- Added `emote` and `emote convert` command\n- Commands with subcommands now show a number behind them in the help menus"
+    news = f"- Added `emote` and `emote convert` command\n- Commands with subcommands now show a number behind them in the help menus\n-warn commands now only require `manage_messages`"
     if ctx.invoked_subcommand is None:
         e = discord.Embed(title=f'help cmd | prefix: `{ctx.prefix}`', description=botdesc, color=config.color)
         e.add_field(name='info', value=f'**Developers:** [Bluewy](https://discord.com/users/698080201158033409) | [Toothless](https://discord.com/users/341988909363757057)\n{stats}\n{discordpy}', inline=False)
