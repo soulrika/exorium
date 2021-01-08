f619f60526fbf5f4a883872a4ebcb95e3b30cdeb
Bluewy <bluewyechache@gmail.com>
2021-01-08 14:26:21 +0100

    Update functions.py

--- outsources/functions.py
+++ outsources/functions.py
@@ -14,15 +14,15 @@ async def interactions(ctx, members, reason, type, ending, typespecial):
     await ctx.send(embed=embed)
 
 
-async def logging(ctx, type, bot):
-    print(f"Command \"{type}\" was used in {ctx.channel} (guild {ctx.guild}) by {ctx.message.author}")
-    embed = discord.Embed(title="exorium command log", color=config.color)
-    embed.add_field(name="Command", value=f"`\"{type}\"`", inline=True)
-    embed.add_field(name="Author", value=f"`{ctx.message.author}`", inline=True)
-    embed.add_field(name="Guild", value=f"`{ctx.guild}`", inline=True)
-    embed.add_field(name="Channel", value=f"`{ctx.channel}`", inline=True)
-    channel = bot.get_channel(755138117488345118)
-    await channel.send(embed=embed)
+# async def logging(ctx, type, bot):
+#     print(f"Command \"{type}\" was used in {ctx.channel} (guild {ctx.guild}) by {ctx.message.author}")
+#     embed = discord.Embed(title="exorium command log", color=config.color)
+#     embed.add_field(name="Command", value=f"`\"{type}\"`", inline=True)
+#     embed.add_field(name="Author", value=f"`{ctx.message.author}`", inline=True)
+#     embed.add_field(name="Guild", value=f"`{ctx.guild}`", inline=True)
+#     embed.add_field(name="Channel", value=f"`{ctx.channel}`", inline=True)
+#     channel = bot.get_channel(755138117488345118)
+#     await channel.send(embed=embed)
 
 async def logging(ctx, type, bot):
     print(f"\"{type}\" used in {ctx.channel} | {ctx.guild} by {ctx.message.author}")
