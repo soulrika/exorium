09a7644ba5e04f6ac6b617d549647628d536ddcc
Bluewy <bluewyechache@gmail.com>
2021-01-19 16:41:57 +0100

    Update main.py

--- main.py
+++ main.py
@@ -608,10 +608,10 @@ async def bonk(ctx, members: commands.Greedy[discord.Member], *, reason="bad!"):
     if str(ctx.message.author.id) in str(members):
         await ctx.send("You can't bonk yourself, derp!")
         return
-    if str(698080201158033409) in members:
+    if str(698080201158033409) in str(members):
         await ctx.send("**Bonking this kind and kyoot dragon IS NOT ALLOWED**")
         return
-    if str(341988909363757057) in members:
+    if str(341988909363757057) in str(members):
         await ctx.send("**Bonking this kind and kyoot dragon IS NOT ALLOWED**")
     else:
          await functions.interactions(ctx, members, reason, "bonk", "how mean", "bonked")
