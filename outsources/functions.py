5dd2ffe635709bb6930b4405b230bca7d3d18d8e
Bluewy <bluewyechache@gmail.com>
2021-01-24 16:03:32 +0100

    Update functions.py

--- outsources/functions.py
+++ outsources/functions.py
@@ -11,7 +11,7 @@ async def interactions(ctx, members, reason, type, ending, typespecial):
     if not (members):
         return await ctx.send(f"Please specify at least one cutie to {type}!")
     embed = discord.Embed(title="", color=config.color, description=(ctx.message.author.mention + " " + f"**{typespecial}**" + " " + '**,** '.join(x.mention for x in members) + f"**, {ending}!**\nFor: " + reason))
-    embed.add_footer(text="Check info about the gifs/images with exo help social, and help us!")
+    embed.set_footer(text="Check info about the gifs/images with exo help social, and help us!")
     embed.set_image(url=GIF)
     await ctx.send(embed=embed)
 
