9922c884e95f175c88ae741948d61d72449429dd
Bluewy <bluewyechache@gmail.com>
2021-02-02 21:50:01 +0100

    Update error.py

--- cogs/error.py
+++ cogs/error.py
@@ -15,6 +15,8 @@ class error(commands.Cog, name="Error"):
             e.description = f"{ctx.message.author} had an error while using a command:\n`Required arguments were not specified.`"
             channel = self.bot.get_channel(790239054868381697)
             await channel.send(embed=e)
+        
+        
         if isinstance(error, commands.MissingPermissions):
             ie = discord.Embed(color=config.red)
             ie.add_field(name='error while processing', value='You do not have the sufficient permissions.')
@@ -23,6 +25,7 @@ class error(commands.Cog, name="Error"):
             e.description = f"{ctx.message.author} had an error while using a command:\n`User permissions are too low.`"
             channel = self.bot.get_channel(790239054868381697)
             await channel.send(embed=e)
+        
         if isinstance(error, commands.NotOwner):
             ie = discord.Embed(color=config.orange)
             ie.add_field(name='error while processing', value='Only bot owners can use this command.')
@@ -31,6 +34,7 @@ class error(commands.Cog, name="Error"):
             e.description = f"{ctx.message.author} had an error while using a command:\n`Command can only be used by bot owners.`"
             channel = self.bot.get_channel(790239054868381697)
             await channel.send(embed=e)
+        
         if isinstance(error, commands.CommandOnCooldown):
             cdamount = '{:.2f}'.format(error.retry_after)
             ie = discord.Embed(color=config.red)
@@ -40,6 +44,7 @@ class error(commands.Cog, name="Error"):
             e.description=f"Cooldown (`{cdamount}s`) occured for {ctx.message.author} in {ctx.guild.name} (`{ctx.guild.id}`)."
             channel = self.bot.get_channel(790239054868381697)
             await channel.send(embed=e)
+        
         if isinstance(error, discord.ext.commands.errors.CommandInvokeError):
             ie = discord.Embed(title="⚠️ An error occured", color=config.red)
             ie.description="```{}```".format(error)
@@ -48,6 +53,7 @@ class error(commands.Cog, name="Error"):
             e = discord.Embed(title="⚠️ An error occured", color=config.red)
             e.description="```{}```".format(error)
             await channel.send(content=f"<@&755070139325743226> | {ctx.message.author} | {ctx.guild.name} (`{ctx.guild.id}`)", embed=e)
+        
         if isinstance(error, discord.ext.commands.errors.Forbidden(response, message)):
             ie = discord.Embed(title="⚠️ An error occured", color=config.red)
             ie.description="```{} {} {}```".format(error,response,message)
