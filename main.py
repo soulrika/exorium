e432ab479b50aecc0e99a5fddc36feb62fb3505f
Bluewy <bluewyechache@gmail.com>
2021-02-04 13:59:54 +0100

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ intents = discord.intents.none()
 
 startTime = datetime.now().timestamp()
 
-bot = commands.Bot(command_prefix=["exo ", "Exo ", "e!", "alexa "], case_insensitive=True, intents=intents)  # sets the bot prefix
+bot = commands.Bot(command_prefix=["exo ", "Exo ", "e!", "alexa "], case_insensitive=True)  # sets the bot prefix
 bot.remove_command('help')  # removes the default discord.py help command
 
 
