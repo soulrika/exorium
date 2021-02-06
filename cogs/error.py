529fc416669566b37a2a329accffdde3800d9473
Bluewy <bluewyechache@gmail.com>
2021-02-06 14:24:14 +0100

    Update error.py

--- cogs/error.py
+++ cogs/error.py
@@ -1,16 +1,1 @@
-import discord, config
-from discord.ext import commands
 
-class error(commands.Cog, name="Error"):
-    def __init__(self, bot):
-        self.bot = bot
-        
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArguments):
-            await ctx.send('You are missing required arguments')
-        else:
-            await ctx.send(embed=discord.Embed(title="An error occured", description=str(error)))
-        
-def setup(bot):
-    bot.add_cog(error(bot))
