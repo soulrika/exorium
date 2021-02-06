d75dfded0ab60a04e34ce75b581daa0a72a98884
Bluewy <bluewyechache@gmail.com>
2021-02-06 14:33:10 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,9 +27,4 @@ for extension in config.extensions:
         tbe = "".join(tb) + ""
         print(f'[WARNING] Could not load extension {extension}: {tbe}')
 
-    @commands.Cog.listener()
-    async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.MissingRequiredArguments):
-            await ctx.send('You are missing required arguments')
-
 bot.run(config.token)
