89b0caf2653eee738fd61c2f82d26cab9ade7eec
Bluewy <bluewyechache@gmail.com>
2021-02-22 14:58:38 +0100

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -169,7 +169,7 @@ __**System**__
             hs = "Brilliance"
         elif user.public_flags.hypesquad_bravery == True:
             hs = "Bravery"
-        elif:
+        else:
             hs = "None"
 
         e.add_field(name="__**Public flags**__",
