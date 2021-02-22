0ae5fd1a28d541924db8abf22deef73fe37d512f
Bluewy <bluewyechache@gmail.com>
2021-02-22 12:28:18 +0100

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -122,10 +122,8 @@ __**System**__
                     value=f"**Owner:** {gu.owner}\n**Owner ID:**\n`{gu.owner.id}`\n**Created:** {default.date(ctx.guild.created_at)}\n**Region:** {str(gu.region)}\n**MFA: **{mfa}\n**Verification:** {str(ctx.guild.verification_level).capitalize()}", inline=True)
         e.add_field(name="__**Server features**__",
                     value=features, inline=True)
-        e.add_field(name="__**Server members**__",
-                    value=f"**Humans:** {len(gu.bots)}\n**Bots:** {len(gu.humans)}", inline=True)
-        e.add_field(name="__**Server channels**__",
-                    value=f"**Text:** {len(gu.text_channels)}\n**Voice:** {len(gu.voice_channels)}", inline=True)
+        e.add_field(name="__**Others**__",
+                    value=f"**Humans:** {len(gu.bots)}\n**Bots:** {len(gu.humans)}\n**Text:** {len(gu.text_channels)} channels\n**Voice:** {len(gu.voice_channels)} channels", inline=False)
         e.set_author(name=f"{gu.name} information", icon_url=gu.icon_url)
         e.set_footer(text=f"Guild ID: {gu.id}")
         e.set_thumbnail(url=gu.icon_url)
