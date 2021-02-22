622f06a2f3a960ac2c5b658789f7c934f203f1e1
Bluewy <bluewyechache@gmail.com>
2021-02-22 14:37:56 +0100

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -152,13 +152,19 @@ __**System**__
             botv = "Yes"
         else:
             botv = "No"
-
+    
         e = discord.Embed(color=config.color)
         e.set_author(name=user.name, icon_url=user.avatar_url)
         e.add_field(name="__**Generic information**__",
-                    value=f"**Username:** {user}\n**User ID:** {user.id}\n**Created:** {default.date(user.created_at)}\n**Joined:** {default.date(user.joined_at)}\n**Avatar URL:** [Click here]({user.avatar_url})\n**Application:** {appl}\n**Bot:** {bot} (verified: {botv})", inline=False)
+                    value=f"**Username:** {user}\n**User ID:** {user.id}\n**Created:** {default.date(user.created_at)}\n**Joined:** {default.date(user.joined_at)}\n**Avatar URL:** [Click here]({user.avatar_url})\n**Application:** {appl}\n**Bot:** {bot} (verified: {botv})", inline=True)
+        
+        if user.public_flags.staff == True:
+            staff = "Yes"
+        else:
+            staff = "No"
+
         e.add_field(name="__**Public flags**__",
-                    value=f"**Discord staff:** {ctx.author.public_flags.staff}\n**Hypesquad:** {ctx.author.public_flags.hypesquad_balance}")
+                    value=f"**Discord staff:** {staff}\n**Hypesquad:** {ctx.author.public_flags.hypesquad", inline=True)
         e.set_thumbnail(url=user.avatar_url)
         await ctx.send(embed=e)
         
